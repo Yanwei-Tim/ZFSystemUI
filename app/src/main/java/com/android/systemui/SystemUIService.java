fb3a0418817b3898a8cd16ffe14e6d4a89cd318e
Tim <1352430027@qq.com>
2019-01-14 18:16:12 +0800

    update ui

--- app/src/main/java/com/android/systemui/SystemUIService.java
+++ app/src/main/java/com/android/systemui/SystemUIService.java
@@ -4,38 +4,13 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
-
 public class SystemUIService extends Service {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        ((SystemUIApplication) getApplication()).startServicesIfNeeded();
+    public SystemUIService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    @Override
-    protected void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
-        SystemUI[] services = ((SystemUIApplication) getApplication()).getServices();
-        if (args == null || args.length == 0) {
-            for (SystemUI ui: services) {
-                pw.println("dumping service: " + ui.getClass().getName());
-                ui.dump(fd, pw, args);
-            }
-        } else {
-            String svc = args[0];
-            for (SystemUI ui: services) {
-                String name = ui.getClass().getName();
-                if (name.endsWith(svc)) {
-                    ui.dump(fd, pw, args);
-                }
-            }
-        }
+        // TODO: Return the communication channel to the service.
+       return null;
     }
 }
